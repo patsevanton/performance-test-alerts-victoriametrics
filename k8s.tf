3b6ec7eb7206e6c7ae8519e28db7633e5ac15dec
Anton Patsev <patsev.anton@gmail.com>
2026-08-14 11:24:44 +0600

    Increase node group size to 22

--- k8s.tf
+++ k8s.tf
@@ -63,7 +63,7 @@ resource "yandex_kubernetes_node_group" "k8s-node-group" {
     fixed_scale {
       # kubectl показал Pending у vmalert из-за `Insufficient cpu` (request=4 core на pod, на части нод уже ~96% по requests).
       # Добавляем 1 ноду для гарантированного размещения тяжёлых monitoring pod'ов при rollout/reconcile.
-      size = 21 # увеличить кол-во нод
+      size = 22 # увеличить кол-во нод
     }
   }
 
